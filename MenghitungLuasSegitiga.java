c4da21b35fb988aab2a1569317e44a131942a464
ayudian12 <70583611+ayudian12@users.noreply.github.com>
2020-09-14 14:13:41 +0700

    Add files via upload

---
+++ MenghitungLuasSegitiga.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author ASUS A407M
+ */
+public class MenghitungLuasSegitiga {
+ public static void main (String [] args) {
+     double a=10, t=20, luas;
+     //*perhitungan
+         luas=a*t/2;
+     //*output
+     System.out.println("===PROGRAM MENGHITUNG LUAS SEGITIGA===");
+     System.out.println("Alas\t= "+a+"cm");
+     System.out.println("Tinggi\t= "+t+"cm");
+     System.out.println("Luas\t= AxT/2" );
+     System.out.println("\t= "+luas+"cm2");
+    
+    }
+}
